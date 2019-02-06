8b6617858c7aead4ad9adff7040e5fb8c5fb3d49
orenchuk <orenchuky@ucu.edu.ua>
2019-02-06 17:20:44 +0200

    Initial Commit

---
+++ whatWasOrdered/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whatWasOrdered
+//
+//  Created by Yevhenii Orenchuk on 2/6/19.
+//  Copyright © 2019 Yevhenii Orenchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
